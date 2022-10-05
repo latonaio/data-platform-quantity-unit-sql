36d8abb36913ae284ce7d90db6d072b6f6fb7fe3
Yurino Oga <yurino.o@latona.io>
2022-10-05 22:01:55 +0900

    release

--- data-platform-quantity-unit-sql-text-data.sql
+++ data-platform-quantity-unit-sql-text-data.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `data_platform_quantity_unit_text_data`
 (
-    `QuantityUnit`                 varchar(2) NOT NULL,  
+    `QuantityUnit`                 varchar(10) NOT NULL,  
     `Language`                     varchar(3) NOT NULL,
     `QuantityUnitName`             varchar(100) DEFAULT NULL,
     PRIMARY KEY (`QuantityUnit`, `Language`)
-    CONSTRAINT `DataPlatformQuantityUnitData_fk` FOREIGN KEY (`QuantityUnit`) REFERENCES `data_platform_quantity-unit-quantity-unit_data` (`QuantityUnit`)
+    CONSTRAINT `DataPlatformQuantityUnitData_fk` FOREIGN KEY (`QuantityUnit`) REFERENCES `data_platform_quantity_unit_quantity_unit_data` (`QuantityUnit`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
