490638b62cad622bfd60a0f5864710734f18a3fb
Yurino Oga <yurino.o@latona.io>
2022-10-05 22:04:56 +0900

    release

--- data-platform-quantity-unit-sql-quantity-unit-data.sql
+++ data-platform-quantity-unit-sql-quantity-unit-data.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `data_platform_quantity_unit_quantity_unit_data`
 (
-    `QuantityUnit`                  varchar(10) NOT NULL,  
+    `QuantityUnit`                varchar(3) NOT NULL,  
     PRIMARY KEY (`QuantityUnit`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
--- data-platform-quantity-unit-sql-text-data.sql
+++ data-platform-quantity-unit-sql-text-data.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `data_platform_quantity_unit_text_data`
 (
-    `QuantityUnit`                 varchar(10) NOT NULL,  
+    `QuantityUnit`                 varchar(3) NOT NULL,  
     `Language`                     varchar(3) NOT NULL,
     `QuantityUnitName`             varchar(100) DEFAULT NULL,
-    PRIMARY KEY (`QuantityUnit`, `Language`)
-    CONSTRAINT `DataPlatformQuantityUnitData_fk` FOREIGN KEY (`QuantityUnit`) REFERENCES `data_platform_quantity_unit_quantity_unit_data` (`QuantityUnit`)
+    PRIMARY KEY (`QuantityUnit`, `Language`),
+    CONSTRAINT `DataPlatformQuantityUnitTextData_fk` FOREIGN KEY (`QuantityUnit`) REFERENCES `data_platform_quantity_unit_quantity_unit_data` (`QuantityUnit`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
